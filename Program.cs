02b529004cf94e86d7920cd079ce8b6197008eb8
Kirill <147234461+KirillCh22@users.noreply.github.com>
2023-12-19 22:31:00 +0300

    Создал функцию для вывода строки с определенным количеством сиволов

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
-﻿
+﻿void CountingChars(string[] array, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        string str_Val = array[i];
+        if (str_Val.Length <= 3)
+        {
+            Console.Write( str_Val + ", ");
+        }
+    }
+}
+
+
 void Main()
 {
     string[] array = new string[] {"Hello", "4", "world", ":-)", "---"};
     int len_array = array.Length;
     Console.WriteLine();
+    CountingChars(array, len_array);
 
 }
 
