386ba3a91cdf395aba56cb7bfbacfcd642cf94d6
Kirill <147234461+KirillCh22@users.noreply.github.com>
2023-12-19 22:33:08 +0300

    Добавил вывод в терминал для удобства чтения

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿void CountingChars(string[] array, int length)
 {
+    Console.Write("New Array - [");
     for (int i = 0; i < length; i++)
     {
         string str_Val = array[i];
@@ -8,6 +9,7 @@
             Console.Write( str_Val + ", ");
         }
     }
+    Console.Write("]");
 }
 
 
